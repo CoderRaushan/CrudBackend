02a73cf658ec054f91955813c8f814d3f0663d4f
CoderRaushan <raushankumar23082004@gmail.com>
2025-02-24 23:15:55 +0530

    cors udpdated

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRoute from "./routes/userRoute.js";
 dotenv.config();
 const app=express();
 app.use(userRoute);
 app.use(express.json());
+app.use(cors("http://localhost:5173"));
 const port=process.env.PORT;
 const mongodbrui=process.env.mongodburi;
 mongoose.connect(mongodbrui).then(()=>
